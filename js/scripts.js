b8897b0af237c361ff204a68256f521358b6af70
Jimmy McNamara <jmcnamara44@gmail.com>
2018-04-06 11:04:24 -0700

    show options for size and toppings on html, as well as a 'review order' button. In js have a Pizza constructor with toppings and size, as well as the beginning of a price prototype.  include jQuery functionality so when user clicks review order the user input will be collected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 //business logic
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
 
-
+Pizza.prototype.price = function() {
+  this.toppings.length + this.size;
+}
 
 //user interface logic
 
 $(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+    var checkedToppings;
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      checkedToppings = $(this).val();
+      alert(checkedToppings);
+    });
+    var checkedSize = $("#size").val();
+    // var newPizza = new Pizza(checkedToppings, checkedSize);
 
+    $("#price").show();
+  });
 });
