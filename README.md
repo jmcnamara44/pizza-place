067fe9de8dd7ee248f5ad0cd3b1da3097638c3ad
Jimmy McNamara <jmcnamara44@gmail.com>
2018-04-06 15:06:03 -0700

    include a README.md file. Comment out the 'add another pizza button' functionality as it is not currently working.  Continue to work on it.

---
+++ README.md
@@ -0,0 +1,52 @@
+# _Pizza-place_
+
+#### _Webpage that gives a user a choice of sizes and pizza toppings and when they submit the form they will receive a price and an order summary. 04/06/18_
+
+#### By _**Jimmy McNamara**_
+
+## Description
+
+_Practice using JS and jQuery, specifically objects, constructors, and prototypes to take a pizza order and come up with a formula for the price._
+
+## Project Specs
+
+_When user clicks submit they receive a price._
+_Price is updated when user selects a pizza size._
+_Price is updated when user adds toppings._
+_Price is updated to reflect changes to both toppings and size._
+_When the price of the order is clicked an order summary appears for the user._
+
+## Project Specs not yet met
+_If user wants to order another pizza they click the add another pizza button and they have the option to select another size and topping._
+_When user hits submit the price is reflected for both orders._
+_When user clicks on the price the summary shows the details for both orders._
+
+
+
+## Setup/Installation Requirements
+
+* _Clone repository from GitHub_
+* _Open index.html file into your browser of choice_
+
+## Known Bugs
+
+_Not able to get the 'add another pizza button to work._
+_Spacing for the order summary of toppings needs to be cleaned up, as well as capitalizing the first letter of each word._
+
+## Support and contact details
+
+_Contact Jimmy with any questions or comments_
+
+## Technologies Used
+
+_HTML_
+_CSS_
+_Bootstrap_
+_JavaScript_
+_jQuery_
+
+### License
+
+*Licensed through the MIT open resource agreement*
+
+Copyright (c) 2018 **_Jimmy McNamara_**
--- js/scripts.js
+++ js/scripts.js
@@ -13,32 +13,47 @@ var checkedSize;
 var checkedToppings;
 var newPizza;
 var pizzaSize;
+var finalSizes= [];
 //user interface logic
 $(document).ready(function() {
-  $("#add-pizza").click(function() {
-    $(".selection").append('<div class="selection">' +
-                              '<select class="form-control" id="size">' +
-                                '<option value="0"></option>' +
-                                '<option value="10">Small (12\'\')</option>' +
-                                '<option value="14">Medium (14\'\')</option>' +
-                                '<option value="18">Large (16\'\')</option>' +
-                              '</select>' +
-                              '<h4>Please choose from the following options if you would like to add a topping (check all that apply)</h4>' +
-                              '<input type="checkbox" name="toppings" value="extra-cheese">Extra Cheese<br>' +
-                              '<input type="checkbox" name="toppings" value="sausage">Sausage<br>' +
-                              '<input type="checkbox" name="toppings" value="chicken">Chicken<br>' +
-                              '<input type="checkbox" name="toppings" value="anchovy">Anchovies<br>' +
-                              '<input type="checkbox" name="toppings" value="sausage">Sausage<br>' +
-                              '<input type="checkbox" name="toppings" value="pepperonni">Pepperonni<br>' +
-                              '<input type="checkbox" name="toppings" value="pineapple">Pineapple<br>' +
-                              '<input type="checkbox" name="toppings" value="beets">Beets<br>' +
-                            '</div>)');
-  });
+  // $("#add-pizza").click(function() {
+  //   $(".selection").append('<div class="selection">' +
+  //                             '<select class="form-control" id="size">' +
+  //                               '<option value="0"></option>' +
+  //                               '<option value="10">Small (12\'\')</option>' +
+  //                               '<option value="14">Medium (14\'\')</option>' +
+  //                               '<option value="18">Large (16\'\')</option>' +
+  //                             '</select>' +
+  //                             '<h4>Please choose from the following options if you would like to add a topping (check all that apply)</h4>' +
+  //                             '<input type="checkbox" name="toppings" value="extra-cheese">Extra Cheese<br>' +
+  //                             '<input type="checkbox" name="toppings" value="sausage">Sausage<br>' +
+  //                             '<input type="checkbox" name="toppings" value="chicken">Chicken<br>' +
+  //                             '<input type="checkbox" name="toppings" value="anchovy">Anchovies<br>' +
+  //                             '<input type="checkbox" name="toppings" value="sausage">Sausage<br>' +
+  //                             '<input type="checkbox" name="toppings" value="pepperonni">Pepperonni<br>' +
+  //                             '<input type="checkbox" name="toppings" value="pineapple">Pineapple<br>' +
+  //                             '<input type="checkbox" name="toppings" value="beets">Beets<br>' +
+  //                           '</div>)');
+  // });
 
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
-
-
+    // not able to have final results show both sizes of pizza selected or tally the total cost of the order.  only reflects results with initial choice.
+    // $(".selection").each(function() {
+    //
+    //   finalToppings = [];
+    //   $("input:checkbox[name=toppings]:checked").each(function() {
+    //     checkedToppings = $(this).val();
+    //     finalToppings.push(checkedToppings);
+    //   });
+    //
+    //   checkedSize = parseInt($("#size").val());
+    //   newPizza = new Pizza(finalToppings, checkedSize);
+    //
+    //   $("#final-price").text(newPizza.price())
+    //   $("#summary").hide();
+    //   $("#result").show();
+    // })
 
     finalToppings = [];
     $("input:checkbox[name=toppings]:checked").each(function() {
@@ -64,6 +79,8 @@ $(document).ready(function() {
         return "Large";
       }
     }
+
+
     $("#summary").show();
     $("#pizza-size").text(pizzaSize());
     $("#pizza-toppings").text(finalToppings); //ideally would like the spacing to be better with this text output.//
